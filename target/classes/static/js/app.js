67325ef3f4b9d61c31619b6c8c2b48bee803228d
MPulidoM <118181543+mpulidom@users.noreply.github.com>
2023-10-24 23:02:38 -0500

    PRUEBA DE BOTONES-Click UNO APIMOCK
    Co-authored-by: XimenaRodriguez20 <123812926+ximenarodriguez20@users.noreply.github.com>
    Co-authored-by: SantiagoMelo0104 <123812833+santiagomelo0104@users.noreply.github.com>
    Co-authored-by: JordyBautista10 <123812969+jordybautista10@users.noreply.github.com>
    Co-authored-by: Juc28 <erika.castro.romero28@gmail.com>

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -1,26 +1,48 @@
-var api=apimock
+var api=apimock;
 blueprintOpen = false
 
 var BlueprintsModule = (function(){
+    var plano="";
+    var autor="";
+    var point=[];
+    var currentBlueprint;
+    var points=[];
+    var graficarPlano = function(funcion){
 
-    var graficarPlano = function(nameAutor,namePlano){
         blueprintOpen = true;
         var c = document.getElementById("myCanvas");
         var ctx = c.getContext("2d");
         ctx.beginPath()
         ctx.clearRect(0, 0, c.width, c.height);
-        console.log(c.width, c.height)
-        funcion=getBlueprintsByNameAndAuthor(api.getBlueprintsByAuthor(nameAutor,getBlueprints),namePlano);
-        funcion.map(function(f){
+        console.log(c.width, c.height);
+        currentBlueprint=funcion
+        funcion['points'].map(function(f){
             console.log(f.x)
             ctx.lineTo(f.x,f.y);
             ctx.stroke();
         })
+        point.map(function(f){
+            console.log("memoria puntos "+ f.x)
+            ctx.lineTo(f.x,f.y);
+            ctx.stroke();
+        })
         ctx.closePath()
-        console.log(getBlueprintsByNameAndAuthor(api.getBlueprintsByAuthor(nameAutor,getBlueprints),namePlano))
-        $("#blueprintname").text(namePlano)
-    };
+        $("#blueprintname").text(funcion['name'])
+        plano=funcion['name'];
+        autor=funcion['author'];
 
+
+    };
+    var graficarPlano2 = function(){
+
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath()
+        console.log(c.width, c.height)
+
+        ctx.closePath()
+
+    };
     var getBlueprints = function(funcion){
         return funcion;
     };
@@ -30,12 +52,14 @@ var BlueprintsModule = (function(){
         funcion.map(function(f){
             if(f.name==name){
                 points=f.points
+                currentBlueprint = f
             }
         });
         return points;
     };
 
     var getByAuthor = function (funcion) {
+
         return funcion.map(function(f){
             return {name:f.name,points:Object.keys(f.points).length};
         });
@@ -43,16 +67,19 @@ var BlueprintsModule = (function(){
 
 
     var generarTable = function (name,funcion) {
+        console.log(funcion)
+        var fun=getByAuthor(funcion);
+        console.log("esta fue");
         $("#cuerpo").html("");
         var total=0
         $("#totalPoints").text(total)
-        funcion.map(function(f) {
+        fun.map(function(f) {
             $('#cuerpo')
                 .append(
                     `<tr>
 					<td>`+f.name+`</td>
 					<td>`+f.points+`</td>`+
-                    "<td><form><button type='button' class='btn btn-primary' onclick='BlueprintsModule.graficarPlano( \"" +
+                    "<td><form><button type='button' class='btn btn-primary' onclick='BlueprintsModule.init_canvas( \"" +
                     name +
                     '" , "' +
                     f.name +
@@ -65,16 +92,84 @@ var BlueprintsModule = (function(){
         $("#authorname").text(name+"'s")
     };
 
+    var initMouse = function(){
+        console.info('initialized');
+        var canvas = document.getElementById("myCanvas"),
+            context = canvas.getContext("2d");
 
+
+        if (window.PointerEvent) {
+            canvas.addEventListener("pointerdown", draw, false);
+        }
+    };
+
+
+    var draw = function () {
+        if (blueprintOpen){
+            var canvas = document.getElementById("myCanvas"),
+                context = canvas.getContext("2d");
+            var offsetleft =  parseInt(getOffset(canvas).left, 10);
+            var offsettop =  parseInt(getOffset(canvas).top, 10);
+            var x = event.pageX-offsetleft;
+            var y = event.pageY-offsettop;
+            var cordenadas={"x":x,"y":y};
+            api.getBlueprintsByNameAndAuthor(autor,plano,graficarPlano);
+            point.push(cordenadas)
+
+            //api.repaintPoints(autor,plano,graficarPlano2)
+
+        }
+    };
+
+    var updateBlueprint=  function(){
+        point.map(function(f){
+            currentBlueprint.points.push(f)
+        });
+        point=[]
+        api.setBlueprint(autor,plano,JSON.stringify(currentBlueprint),run)
+    }
+
+
+    var init_canvas= function (nombre,nombrep) {
+        $("#cuerpoSaveUpdate").css("visibility", "visible");
+        point=[];
+        api.getBlueprintsByNameAndAuthor(nombre,nombrep,graficarPlano)
+
+    };
+
+    var getOffset = function (obj) {
+        var offsetLeft = 0;
+        var offsetTop = 0;
+        do {
+            if (!isNaN(obj.offsetLeft)) {
+                offsetLeft += obj.offsetLeft;
+            }
+            if (!isNaN(obj.offsetTop)) {
+                offsetTop += obj.offsetTop;
+            }
+        } while(obj = obj.offsetParent );
+        return {left: offsetLeft, top: offsetTop};
+    };
+
+
+    var newBlueprint = function(){
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height);
+        graficarPlano("","");
+    }
 
     var run = function() {
         var nameAutor = $('#autor').val();
-
-        generarTable(nameAutor,api.getBlueprintsByAuthor(nameAutor,getByAuthor));
+        api.getBlueprintsByAuthor(nameAutor,generarTable);
     }
 
     return {
         run: run,
-        graficarPlano: graficarPlano
+        initMouse: initMouse,
+        graficarPlano: graficarPlano,
+        init_canvas:init_canvas,
+        updateBlueprint: updateBlueprint,
+        newBlueprint: newBlueprint
     };
 })();
